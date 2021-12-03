81b2b021016e88bc702a85e3816d6af6b1e48acc
Gjorgji Tashkovski <taskovskig@gmail.com>
2021-12-03 15:10:05 +0100

    eks: rename node groups

--- eks/eks.sh
+++ eks/eks.sh
@@ -7,6 +7,6 @@ export AWS_DEFAULT_REGION=eu-central-1
 eksctl create cluster --version=1.21 --name=${AWS_EKS_CLUSTER_NAME} --nodes=1 --managed --region=${AWS_DEFAULT_REGION} --node-type t3.medium --asg-access
 
 # Create managed node groups
-eksctl create nodegroup --cluster=${AWS_EKS_CLUSTER_NAME} --region=${AWS_DEFAULT_REGION} --managed --spot --name=spot-node-group-2vcpu-8gb --instance-types=t3.small --nodes-min=1 --nodes-max=5 --asg-access
+eksctl create nodegroup --cluster=${AWS_EKS_CLUSTER_NAME} --region=${AWS_DEFAULT_REGION} --managed --spot --name=spot-node-group-2vcpu-2gb --instance-types=t3.small --nodes-min=1 --nodes-max=5 --asg-access
 
-eksctl create nodegroup --cluster=${AWS_EKS_CLUSTER_NAME} --region=${AWS_DEFAULT_REGION} --managed --spot --name=spot-node-group-4vcpu-16gb --instance-types=t3.medium --nodes-min=0 --nodes-max=5 --asg-access
+eksctl create nodegroup --cluster=${AWS_EKS_CLUSTER_NAME} --region=${AWS_DEFAULT_REGION} --managed --spot --name=spot-node-group-2vcpu-4gb --instance-types=t3.medium --nodes-min=0 --nodes-max=5 --asg-access
